9cce4dd7e5581936ecd1ecdb1f55c994540d5e6b
haterain0203 <haterain0203@gmail.com>
2023-03-23 17:21:43 +0900

    曜日取得のプロバイダー追加

--- lib/date_controller.dart
+++ lib/date_controller.dart
@@ -1,3 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
-final todayProvider = Provider((ref) => DateTime.now);
+final todayProvider = Provider<DateTime>((ref) => DateTime.now());
+
+final dayOfWeekProvider = Provider<String>((ref) {
+  final today = ref.watch(todayProvider);
+  final dayOfWeek = DateFormat.E('ja').format(today);
+  return dayOfWeek;
+});
