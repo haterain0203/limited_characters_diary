8b3f829b25be45f4b188eea2289b9e9456ac42d8
haterain0203 <haterain0203@gmail.com>
2023-03-23 11:42:47 +0900

    appBar追加

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -6,6 +6,42 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                //TODO 月を変更する処理
+              },
+              child: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+              ),
+            ),
+            //TODO 固定値
+            const Text('2023年3月'),
+            TextButton(
+              onPressed: () {
+                //TODO 月を変更する処理
+              },
+              child: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              //TODO
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
       body: Container(),
     );
   }
