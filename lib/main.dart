e08860ef5d332d3c52ef127988c0e540d7c0d1cf
haterain0203 <haterain0203@gmail.com>
2023-03-25 07:27:44 +0900

    コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'my_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  https: //zenn.dev/flutteruniv_dev/articles/20220607-061331-flutter-isar?redirected=1#isar-%E3%82%A4%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%B3%E3%82%B9%E3%82%92%E7%94%9F%E6%88%90%E3%81%99%E3%82%8B
   var path = '';
   if (!kIsWeb) {
     final dir = await getApplicationSupportDirectory();
