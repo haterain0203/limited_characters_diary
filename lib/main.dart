da1af8e5e50329801a2e68354904e110f0cecc86
haterain0203 <haterain0203@gmail.com>
2023-03-25 07:27:15 +0900

    Isar インスタンスを生成する

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,27 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
+import 'diary/collection/diary.dart';
 import 'my_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var path = '';
+  if (!kIsWeb) {
+    final dir = await getApplicationSupportDirectory();
+    path = dir.path;
+  }
+
+  final isar = await Isar.open(
+    [
+      DiarySchema,
+    ],
+    directory: path,
+  );
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
