aac79cf5d1846c112ae134521f7ff335efec6078
haterain0203 <haterain0203@gmail.com>
2023-04-01 08:35:37 +0900

    TimeZoneの追加と時間設定での通知処理追加

--- lib/local_notification/local_notification_repository.dart
+++ lib/local_notification/local_notification_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class LocalNotificationRepository {
   LocalNotificationRepository({
@@ -20,5 +22,24 @@ class LocalNotificationRepository {
             sound: true,
           );
     }
+  }
+
+  Future<void> scheduledNotification() async {
+    //TODO 起動時1回でいいはずなので記載箇所を要検討
+    tz.initializeTimeZones();
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'scheduled title',
+      'scheduled body',
+      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+            'your channel id', 'your channel name',
+            channelDescription: 'your channel description'),
+      ),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
   }
 }
