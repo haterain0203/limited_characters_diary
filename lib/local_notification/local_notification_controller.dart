2b056b00c74cbab683fa2828fa71ae81e92ce921
haterain0203 <haterain0203@gmail.com>
2023-04-08 07:32:01 +0900

    FutureProvider追加

--- lib/local_notification/local_notification_controller.dart
+++ lib/local_notification/local_notification_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:limited_characters_diary/local_notification/local_notification_repository.dart';
+import 'package:limited_characters_diary/local_notification/local_notification_shared_preferences_repository.dart';
 
 final localNotificationControllerProvider = Provider(
   (ref) => LocalNotificationController(ref: ref),
@@ -14,6 +15,16 @@ final localNotificationRepoProvider = Provider((ref) {
 // 初期値は21:00
 final localNotificationSetTimeProvider =
     StateProvider<TimeOfDay>((ref) => const TimeOfDay(hour: 21, minute: 00));
+
+final localNotificationSharedRepoProvider = Provider(
+  (ref) => LocalNotificationSharedPreferencesRepository(),
+);
+
+// SharedPreferencesにアクセスし、記録された通知時間を取得する
+final localNotificationTimeFutureProvider = FutureProvider((ref) async {
+  final repo = ref.read(localNotificationSharedRepoProvider);
+  await repo.fetchNotificationTime();
+});
 
 class LocalNotificationController {
   LocalNotificationController({required this.ref});
--- lib/local_notification/local_notification_shared_preferences_repository.dart
+++ lib/local_notification/local_notification_shared_preferences_repository.dart
@@ -4,13 +4,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalNotificationSharedPreferencesRepository {
   final notificationTimeStrKey = 'notification_time';
 
-  Future<void> saveNotificationTime(
-      SharedPreferences prefs, TimeOfDay notificationTime) async {
+  Future<void> saveNotificationTime(TimeOfDay notificationTime) async {
+    final prefs = await SharedPreferences.getInstance();
     final notificationTimeStr = notificationTime.toString();
     await prefs.setString(notificationTimeStrKey, notificationTimeStr);
   }
 
-  Future<void> fetchNotificationTime(SharedPreferences prefs) async {
+  Future<void> fetchNotificationTime() async {
+    final prefs = await SharedPreferences.getInstance();
     prefs.getString(notificationTimeStrKey);
   }
 }
