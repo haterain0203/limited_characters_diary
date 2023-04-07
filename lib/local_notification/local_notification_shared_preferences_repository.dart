9efab2dd607ec2bd938e908db09ab618876c95dd
haterain0203 <haterain0203@gmail.com>
2023-04-08 07:19:38 +0900

    Repositoryクラス作成

--- lib/local_notification/local_notification_shared_preferences_repository.dart
+++ lib/local_notification/local_notification_shared_preferences_repository.dart
@@ -1,1 +1,16 @@
-class LocalNotificationSharedPreferencesRepository {}
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalNotificationSharedPreferencesRepository {
+  final notificationTimeStrKey = 'notification_time';
+
+  Future<void> saveNotificationTime(
+      SharedPreferences prefs, TimeOfDay notificationTime) async {
+    final notificationTimeStr = notificationTime.toString();
+    // prefs.setString('notification_time', notificationTimeStr);
+  }
+
+  Future<void> fetchNotificationTime(SharedPreferences prefs) async {
+    prefs.getString(notificationTimeStrKey);
+  }
+}
