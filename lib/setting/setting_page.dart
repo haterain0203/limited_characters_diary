a433937b3eb7fc1cded14ac65df1a2d3106303e3
haterain0203 <haterain0203@gmail.com>
2023-04-10 10:21:34 +0900

    webviewPageの呼び出し

--- lib/setting/setting_page.dart
+++ lib/setting/setting_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:limited_characters_diary/local_notification/local_notification_setting_dialog.dart';
+import 'package:limited_characters_diary/web_view_page.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatelessWidget {
@@ -33,7 +34,15 @@ class SettingPage extends StatelessWidget {
                 leading: const Icon(Icons.mail),
                 title: const Text('問い合わせ'),
                 onPressed: (BuildContext context) {
-                  //TODO GoogleFormへ
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const WebViewPage(
+                        title: '問い合わせ',
+                        url: 'https://yahoo.co.jp',
+                      ),
+                    ),
+                  );
                 },
               ),
               SettingsTile(
