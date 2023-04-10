2a774acd8df2ac2665c10775e9695c307149e77f
haterain0203 <haterain0203@gmail.com>
2023-04-10 10:15:59 +0900

    情報が古かったため、別プロジェクトの内容に修正

--- lib/web_view_page.dart
+++ lib/web_view_page.dart
@@ -1,64 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends HookConsumerWidget {
+class WebViewPage extends StatefulWidget {
   const WebViewPage({
-    required this.flag,
     super.key,
+    required this.title,
+    required this.url,
   });
-
-  final WebFlag flag;
+  final String title;
+  final String url;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isLoading = useState(true);
-    final title = _setTitle(flag);
-    final url = _setURL(flag);
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  bool _isLoading = true;
+  WebViewController? webViewController;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _setWebViewController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          isLoading.value
-              ? const LinearProgressIndicator(
-                  minHeight: 8,
-                )
-              : const SizedBox(),
-          Expanded(
-            child: WebView(
-              initialUrl: url,
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageFinished: (_) {
-                isLoading.value = false;
-              },
+      body: Builder(builder: (context) {
+        if (_isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (_errorMessage != null) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('エラー'),
+                Text(_errorMessage!),
+              ],
             ),
-          ),
-        ],
-      ),
+          );
+        }
+        return WebViewWidget(controller: webViewController!);
+      }),
     );
   }
 
-  String _setTitle(WebFlag flag) {
-    switch (flag) {
-      case WebFlag.contact:
-        return "お問合せ";
-      case WebFlag.termsOfService:
-        return "利用規約";
-      case WebFlag.privacyPolicy:
-        return "プライバシーポリシー";
-    }
+  void _setWebViewController() {
+    webViewController = WebViewController()
+      ..setBackgroundColor(Colors.black)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          // ページ読み込み中はインジケータを表示するために、_isLoadingをtrueに
+          onPageStarted: (String url) => _startLoading(),
+          // ページ完了後はインジケータを表示しないよう、_isLoadingをfalseに
+          onPageFinished: (String url) => _endLoading(),
+          // ページ読み込み中にエラーが発生した場合に、エラー内容をUI表示するために、_errorMessageにエラー内容をセットする
+          onWebResourceError: (WebResourceError error) {
+            debugPrint(error.description);
+            _setErrorMessage(error);
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse(
+          widget.url,
+        ),
+      );
   }
 
-  String _setURL(WebFlag flag) {
-    switch (flag) {
-      case WebFlag.contact:
-        return "https://docs.google.com/forms/d/e/1FAIpQLSfWmqr_CnYiZlejcpMryTK4qEpQvP3TzABopwylvUWAazG2qg/viewform?usp=sf_link";
-      case WebFlag.termsOfService:
-        return "https://travelskill.jimdosite.com/%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84/";
-      case WebFlag.privacyPolicy:
-        return "https://travelskill.jimdosite.com/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC/";
-    }
+  void _startLoading() {
+    setState(() {
+      _isLoading = true;
+    });
+  }
+
+  void _endLoading() {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _setErrorMessage(WebResourceError error) {
+    setState(() {
+      _errorMessage = error.description;
+      _isLoading = false;
+    });
   }
 }
