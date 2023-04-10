d11b5b7da424d4de67718258fc1094343d33b5f9
haterain0203 <haterain0203@gmail.com>
2023-04-10 10:59:30 +0900

    repositoryクラスの更新

--- lib/app_version/app_version_repository.dart
+++ lib/app_version/app_version_repository.dart
@@ -1,1 +1,8 @@
-class AppVersionRepository {}
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AppVersionRepository {
+  Future<PackageInfo> fetchPackageInfo() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo;
+  }
+}
