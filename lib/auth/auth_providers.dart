5ae97de843d5be361a3067930c342dda907e1a78
haterain0203 <haterain0203@gmail.com>
2023-04-11 12:47:58 +0900

    authController追加

--- lib/auth/auth_providers.dart
+++ lib/auth/auth_providers.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:limited_characters_diary/auth/auth_controller.dart';
 import 'package:limited_characters_diary/auth/auth_repository.dart';
 
 final authInstanceProvider = Provider((ref) => FirebaseAuth.instance);
@@ -8,3 +9,11 @@ final authInstanceProvider = Provider((ref) => FirebaseAuth.instance);
 final firestoreInstanceProvider = Provider((ref) => FirebaseFirestore.instance);
 
 final authRepoProvider = Provider((ref) => AuthRepository());
+
+final authControllerProvider = Provider(
+  (ref) => AuthController(
+    repo: ref.read(authRepoProvider),
+    auth: ref.read(authInstanceProvider),
+    firestore: ref.read(firestoreInstanceProvider),
+  ),
+);
