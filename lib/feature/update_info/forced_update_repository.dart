d769dd2bf13252b5b3f220fb748c91be7bae9c64
haterain0203 <haterain0203@gmail.com>
2023-04-14 14:03:43 +0900

    updateInfoを購読する

--- lib/feature/update_info/forced_update_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class UpdateInfoRepository {
-  UpdateInfoRepository({required this.updateInfoRef});
-  final CollectionReference<UpdateInfo> updateInfoRef;
-}
---
+++ lib/feature/update_info/update_info_repository.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:limited_characters_diary/feature/update_info/update_info.dart';
+
+class UpdateInfoRepository {
+  UpdateInfoRepository({required this.updateInfoRef});
+  final CollectionReference<UpdateInfo> updateInfoRef;
+
+  Stream<UpdateInfo> subscribedUpdateInfo() {
+    final snapshots = updateInfoRef.snapshots();
+    final updateInfo = snapshots.map((snapshot) {
+      return snapshot.docs.first.data();
+    });
+    return updateInfo;
+  }
+}
