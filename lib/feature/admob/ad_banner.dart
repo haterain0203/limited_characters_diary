9e4f1b7d154d778d9c1492057b94755e5d33650a
haterain0203 <haterain0203@gmail.com>
2023-04-15 15:43:29 +0900

    バナー広告表示

---
+++ lib/feature/admob/ad_banner.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdBanner extends StatelessWidget {
+  const AdBanner({
+    super.key,
+    required this.size, // サイズは利用時に指定
+  });
+  final AdSize size;
+  @override
+  Widget build(BuildContext context) {
+    // AndroidかiOSを前提とする
+    final banner = BannerAd(
+      // サイズ
+      size: size,
+      // 広告ユニットID
+      adUnitId: Platform.isAndroid
+          //TODO 本番用に切り替える
+          ? 'ca-app-pub-3940256099942544/6300978111'
+          : 'ca-app-pub-3940256099942544/2934735716',
+      // イベントのコールバック
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) => print('Ad loaded.'),
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          print('Ad failed to load: $error');
+        },
+        onAdOpened: (Ad ad) => print('Ad opened.'),
+        onAdClosed: (Ad ad) => print('Ad closed.'),
+      ),
+      // リクエストはデフォルトを使う
+      request: const AdRequest(),
+    )
+      // 表示を行うloadをつける
+      ..load();
+    // 戻り値はSizedBoxで包んで返す
+    return SizedBox(
+        width: banner.size.width.toDouble(),
+        height: banner.size.height.toDouble(),
+        child: AdWidget(ad: banner));
+  }
+}
