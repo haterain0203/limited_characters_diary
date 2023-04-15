b1ddd306fa76884db852e7e81533b32d833edce2
haterain0203 <haterain0203@gmail.com>
2023-04-15 13:33:14 +0900

    閉じる専用のダイアログ追加

--- lib/component/dialogs.dart
+++ lib/component/dialogs.dart
@@ -35,3 +35,30 @@ class ForcedUpdateDialog extends StatelessWidget {
     );
   }
 }
+
+class OnlyCloseDialog extends StatelessWidget {
+  const OnlyCloseDialog({
+    required this.title,
+    this.content,
+    super.key,
+  });
+
+  final String title;
+  final Widget? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: content,
+      actions: [
+        StadiumBorderButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          title: '閉じる',
+        ),
+      ],
+    );
+  }
+}
