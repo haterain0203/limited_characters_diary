4a410e7eb9cdf7d8e8d2eb648e168f6d2d2d1945
haterain0203 <haterain0203@gmail.com>
2023-04-16 05:19:29 +0900

    Repositoryクラスの追加

---
+++ lib/feature/admob/ad_repository.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdRepository {
+  BannerAd? bannerAd;
+  InterstitialAd? interstitialAd;
+  int maxFailedToAttempt = 3;
+  int _numInterstitialLoadAttempt = 0;
+
+  // void initBannerAd() {
+  //   bannerAd = BannerAd(
+  //     size: AdSize.banner,
+  //     adUnitId: bannerAdUnitId,
+  //     listener: const BannerAdListener(),
+  //     request: const AdRequest(),
+  //   );
+  // }
+  //
+  // void loadBannerAd() {
+  //   bannerAd?.load();
+  // }
+  //
+  // void dispose() {
+  //   bannerAd?.dispose();
+  // interstitialAd?.dispose();
+  // }
+
+  void initInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: interstitialAdUnitId,
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad) {
+          interstitialAd = ad;
+          _numInterstitialLoadAttempt = 0;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          interstitialAd = null;
+          _numInterstitialLoadAttempt++;
+          if (_numInterstitialLoadAttempt <= maxFailedToAttempt) {
+            initInterstitialAd();
+          }
+        },
+      ),
+    );
+  }
+
+  void loadInterstitialAd() {
+    if (interstitialAd == null) {
+      return;
+    }
+    interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (InterstitialAd ad) {
+      ad.dispose();
+      initInterstitialAd();
+    }, onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError adError) {
+      ad.dispose();
+      initInterstitialAd();
+    });
+    interstitialAd!.show();
+    interstitialAd = null;
+  }
+
+  static String get appId {
+    //TODO 本番用に切り替える
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544~3347511713';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544~1458002511';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      //TODO テスト用IDから本番用に切り替える
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      //TODO テスト用IDから本番用に切り替える
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      //TODO テスト用IDのため追って変更
+      return 'ca-app-pub-3940256099942544/1033173712';
+    } else if (Platform.isIOS) {
+      //TODO テスト用IDのため追って変更
+      return 'ca-app-pub-3940256099942544/4411468910';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+}
