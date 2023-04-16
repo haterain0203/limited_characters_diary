92550d4abccd30ec4e194cea891d1fe8b0d27600
haterain0203 <haterain0203@gmail.com>
2023-04-16 09:39:25 +0900

    コメント追加

--- lib/feature/admob/ad_controller.dart
+++ lib/feature/admob/ad_controller.dart
@@ -4,10 +4,12 @@ class AdController {
   AdController({required this.repo});
   final AdRepository repo;
 
+  //全画面広告の表示
   Future<void> showInterstitialAdd() async {
     await repo.loadInterstitialAd();
   }
 
+  //全画面広告の取得
   Future<void> initInterstitialAdd() async {
     await repo.initInterstitialAd();
   }
