9e147e048cc48f6ef7d0993f15f5bc1f2a66ce4a
haterain0203 <haterain0203@gmail.com>
2023-04-18 16:29:27 +0900

    押下できるよう修正

--- lib/feature/diary/sized_list_tile.dart
+++ lib/feature/diary/sized_list_tile.dart
@@ -2,41 +2,50 @@ import 'package:flutter/material.dart';
 
 class SizedListTile extends StatelessWidget {
   const SizedListTile({
+    required this.onTap,
+    required this.onLongPress,
     this.height,
     this.tileColor,
     required this.leading,
     required this.title,
     super.key,
   });
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
   final double? height;
   final Color? tileColor;
   final Widget leading;
   final Widget title;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          width: double.infinity,
-          height: height ?? 40,
-          child: ColoredBox(
-            color: tileColor ?? Colors.transparent,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: SizedBox(
-                    width: 40,
-                    child: leading,
+        InkWell(
+          onTap: onTap,
+          onLongPress: onLongPress,
+          child: SizedBox(
+            width: double.infinity,
+            height: height ?? 40,
+            child: ColoredBox(
+              color: tileColor ?? Colors.transparent,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: SizedBox(
+                      width: 40,
+                      child: leading,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Expanded(
-                    child: title,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Expanded(
+                      child: title,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
