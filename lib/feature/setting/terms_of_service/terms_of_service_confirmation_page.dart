8c3b3d076f47186122f15f539d3c8d1600eb7226
haterain0203 <haterain0203@gmail.com>
2023-05-01 09:39:48 +0900

    利用規約に同意してもらう画面の作成（他プロジェクトから流用）

---
+++ lib/feature/setting/terms_of_service/terms_of_service_confirmation_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:limited_characters_diary/constant/constant.dart';
+import 'package:limited_characters_diary/list_page.dart';
+import 'package:limited_characters_diary/web_view_page.dart';
+
+class TermsOfServiceConfirmationPage extends StatelessWidget {
+  const TermsOfServiceConfirmationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              //TODO
+              'アプリ名が入ります',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            const Text(
+              '利用規約に同意',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Text('アプリを始めるには利用規約の同意が必要です。'),
+            ),
+            TextButton(
+              child: const Text('利用規約を確認'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute<WebViewPage>(
+                  builder: (context) => const WebViewPage(
+                    title: Constant.termsOfServiceStr,
+                    url: Constant.termsOfServiceUrl,
+                  ),
+                ),
+              ),
+            ),
+            HookConsumer(
+              builder: (context, ref, child) => ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                ),
+                child: const Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text('利用規約に同意'),
+                ),
+                onPressed: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute<ListPage>(
+                      builder: (context) => const ListPage(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
