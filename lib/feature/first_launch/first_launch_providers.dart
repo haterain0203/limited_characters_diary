b4bbf11a5c6d575844b6c72c0d744b3ee889a091
haterain0203 <haterain0203@gmail.com>
2023-05-01 12:08:04 +0900

    providerの修正

--- lib/feature/first_launch/first_launch_providers.dart
+++ lib/feature/first_launch/first_launch_providers.dart
@@ -1,3 +1,15 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:limited_characters_diary/feature/first_launch/first_launch_controller.dart';
+import 'package:limited_characters_diary/feature/first_launch/first_launch_repository.dart';
 
 final isFirstLaunchProvider = StateProvider<bool>((_) => false);
+
+final firstLaunchRepositoryProvider = Provider(
+  (ref) => FirstLaunchRepository(),
+);
+
+final firstLaunchControllerProvider = Provider(
+  (ref) => FirstLaunchController(
+    repo: ref.watch(firstLaunchRepositoryProvider),
+  ),
+);
