4f632b01c416a519c1150dd8fe3409a67ff52d48
haterain0203 <haterain0203@gmail.com>
2023-05-01 12:02:14 +0900

    repository追加

---
+++ lib/feature/first_launch/first_launch_repository.dart
@@ -0,0 +1,19 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FirstLaunchRepository {
+
+  final completedFirstLaunchKey = 'completed_first_launch';
+
+  Future<void> completedFirstLaunch() async {
+    //TODO 共通化
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(completedFirstLaunchKey, true);
+  }
+
+  Future<bool?> isFirstLaunch() async {
+    //TODO 共通化
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(completedFirstLaunchKey);
+  }
+
+}
