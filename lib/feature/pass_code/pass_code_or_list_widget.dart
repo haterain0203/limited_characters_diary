f76eb25b349376a3e724fba5be70a0f74d8d0100
haterain0203 <haterain0203@gmail.com>
2023-05-15 15:55:11 +0900

    コメント修正

--- lib/feature/pass_code/pass_code_or_list_widget.dart
+++ lib/feature/pass_code/pass_code_or_list_widget.dart
@@ -8,6 +8,7 @@ import 'package:limited_characters_diary/list_page.dart';
 import '../admob/ad_providers.dart';
 import '../local_notification/local_notification_providers.dart';
 
+/// パスコードロック画面とListPageを切り替えるためのWidget
 class PassCodeOrListWidget extends HookConsumerWidget {
   const PassCodeOrListWidget({
     super.key,
@@ -16,23 +17,24 @@ class PassCodeOrListWidget extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     useOnAppLifecycleStateChange((previous, current) async {
-      /// バックグラウンドになったタイミングで、ScreenLockを呼び出す
+      /// バックグラウンドになったタイミングで、ScreenLockを表示を管理するフラグをtrueにする
       ///
       /// 最初はresumedのタイミングで呼び出そうとしたが、一瞬ListPageが表示されてしまうため、
       /// inactiveのタイミングで呼び出すこととしたもの
-      if (current == AppLifecycleState.inactive) {
+      if (current == AppLifecycleState.resumed) {
+
+        // パスコード設定がOFFなら処理終了
         if(!ref.read(isSetPassCodeLockProvider)) {
           return;
         }
 
-        // 全画面広告から復帰した際は表示しない
+        // 全画面広告から復帰した際は処理終了
         // 全画面広告表示時にinactiveになるが、そのタイミングではパスコードロック画面を表示したくないため
         if (ref.read(isShownInterstitialAdProvider)) {
           return;
         }
 
-        // 初めて通知設定した際は、端末の通知設定ダイアログによりinactiveになるが、その際は表示しない
-        // isShowScreenLockProviderにて使用
+        // 初めて通知設定した際は、端末の通知設定ダイアログによりinactiveになるが、その際は処理終了
         if (ref.read(isInitialSetNotificationProvider)) {
           // falseに戻さないと、初めて通知設定した後にinactiveにした際にロック画面が表示されない
           ref.read(isInitialSetNotificationProvider.notifier).state = false;
