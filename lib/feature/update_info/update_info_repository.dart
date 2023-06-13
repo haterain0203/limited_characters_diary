6f228840d037c42389fde977e84e519c860207fc
haterain0203 <haterain0203@gmail.com>
2023-06-14 06:14:07 +0900

    updateInfoRepo内のメソッドをupdateInfoServiceに移管

--- lib/feature/update_info/update_info_repository.dart
+++ lib/feature/update_info/update_info_repository.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:limited_characters_diary/feature/update_info/update_info.dart';
 import 'package:limited_characters_diary/feature/update_info/update_info_service.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:version/version.dart';
 
 final updateInfoRepoProvider = Provider(
   (ref) => UpdateInfoRepository(
@@ -23,38 +19,5 @@ class UpdateInfoRepository {
       return snapshot.data()!;
     });
     return updateInfo;
-  }
-
-  //TODO ここも向きが逆転しているかも
-  // 端末のアプリバージョンとfirebaseのバージョンを比較し、firebaseの方が新しい場合、
-  // 強制アップデートを促すダイアログを表示するためのboolを返す
-  Future<bool> requiredUpdate(UpdateInfo updateInfo) async {
-    // firebaseのrequired_updateがfalseならfalseをリターンして終了
-    if (!updateInfo.requiredUpdate) {
-      return false;
-    }
-    // firebaseのrequired_updateがtrueなら強制アップデート判定に進む
-    final currentVersion = await getCurrentVersion();
-    final requiredVersion = await getRequiredVersion(updateInfo);
-    final requiredUpdate = currentVersion < requiredVersion;
-    return requiredUpdate;
-  }
-
-  // Appのバージョン情報の取得
-  Future<Version> getCurrentVersion() async {
-    final info = await PackageInfo.fromPlatform();
-    final currentVersion = Version.parse(info.version);
-    return currentVersion;
-  }
-
-  // firebaseのバージョン情報を取得
-  Future<Version> getRequiredVersion(UpdateInfo updateInfo) async {
-    // iosかandroidかでfirebaseから取得するフィールド名を変更する
-    final os = Platform.isIOS
-        ? updateInfo.requiredIosVersion
-        : updateInfo.requiredAndroidVersion;
-    //Firestoreからアップデートしたいバージョンを取得
-    final newVersion = Version.parse(os);
-    return newVersion;
   }
 }
--- lib/feature/update_info/update_info_service.dart
+++ lib/feature/update_info/update_info_service.dart
@@ -1,8 +1,59 @@
+import 'dart:io';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:limited_characters_diary/feature/update_info/update_info.dart';
 import 'package:limited_characters_diary/feature/update_info/update_info_repository.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:version/version.dart';
 
 import '../firestore/firestore_instance_provider.dart';
+
+final updateInfoServiceProvider = Provider(
+  (ref) => UpdateInfoService(
+    repo: ref.watch(updateInfoRepoProvider),
+  ),
+);
+
+class UpdateInfoService {
+  UpdateInfoService({
+    required this.repo,
+  });
+
+  final UpdateInfoRepository repo;
+
+  //TODO ここも向きが逆転しているかも
+  // 端末のアプリバージョンとfirebaseのバージョンを比較し、firebaseの方が新しい場合、
+  // 強制アップデートを促すダイアログを表示するためのboolを返す
+  Future<bool> requiredUpdate(UpdateInfo updateInfo) async {
+    // firebaseのrequired_updateがfalseならfalseをリターンして終了
+    if (!updateInfo.requiredUpdate) {
+      return false;
+    }
+    // firebaseのrequired_updateがtrueなら強制アップデート判定に進む
+    final currentVersion = await getCurrentVersion();
+    final requiredVersion = await getRequiredVersion(updateInfo);
+    final requiredUpdate = currentVersion < requiredVersion;
+    return requiredUpdate;
+  }
+
+  // Appのバージョン情報の取得
+  Future<Version> getCurrentVersion() async {
+    final info = await PackageInfo.fromPlatform();
+    final currentVersion = Version.parse(info.version);
+    return currentVersion;
+  }
+
+  // firebaseのバージョン情報を取得
+  Future<Version> getRequiredVersion(UpdateInfo updateInfo) async {
+    // iosかandroidかでfirebaseから取得するフィールド名を変更する
+    final os = Platform.isIOS
+        ? updateInfo.requiredIosVersion
+        : updateInfo.requiredAndroidVersion;
+    //Firestoreからアップデートしたいバージョンを取得
+    final newVersion = Version.parse(os);
+    return newVersion;
+  }
+}
 
 final updateInfoRefProvider = Provider((ref) {
   final firestore = ref.watch(firestoreInstanceProvider);
@@ -29,7 +80,7 @@ final forcedUpdateProvider = FutureProvider.autoDispose<bool>((ref) async {
   if (updateInfo.value == null) {
     return false;
   }
-  final repo = ref.watch(updateInfoRepoProvider);
-  final requiredUpdate = await repo.requiredUpdate(updateInfo.value!);
+  final service = ref.watch(updateInfoServiceProvider);
+  final requiredUpdate = await service.requiredUpdate(updateInfo.value!);
   return requiredUpdate;
 });
