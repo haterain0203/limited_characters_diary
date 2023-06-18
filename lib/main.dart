600bcb0ce5fe204241114490a914258c2db82465
haterain0203 <haterain0203@gmail.com>
2023-06-18 14:20:35 +0900

    不要コードの削除

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,6 @@ Future<void> main() async {
   runApp(
     Phoenix(
       child: DevicePreview(
-        enabled: !kReleaseMode,
         builder: (_) => ProviderScope(
           overrides: [
             localNotificationServiceProvider
