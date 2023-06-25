d999af45e0ed3d3a16106cf84eb41d080ff4bf2f
haterain0203 <haterain0203@gmail.com>
2023-06-25 16:21:03 +0900

    dartファイルに修正

---
+++ lib/constant/constant_evnet_name.dart
@@ -0,0 +1,18 @@
+/// analyticsに送信するlogのイベント名
+class ConstantEventName {
+
+  static const inputDiary = 'input_diary';
+  
+  static const updateDiary = 'update_diary';
+  
+  static const deleteDiary = 'delete_diary';
+  
+  static const setNotification = 'set_notification';
+  
+  static const agreeWithTerms = 'agree_with_terms';
+  
+  static const enablePassCodeLock = 'enable_pass_code_lock';
+  
+  static const disablePassCodeLock = 'disable_pass_code_lock'; 
+
+}
