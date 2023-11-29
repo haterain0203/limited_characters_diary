0b5f8498f8d9162ccfd0cf8dcb656bc356acd8ec
haterain0203 <haterain0203@gmail.com>
2023-11-29 14:03:52 +0900

    不要なimport文の削除

--- lib/feature/setting/setting_service.dart
+++ lib/feature/setting/setting_service.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:limited_characters_diary/feature/app_info/app_info_service.dart';
 import 'package:limited_characters_diary/feature/auth/auth_service.dart';
