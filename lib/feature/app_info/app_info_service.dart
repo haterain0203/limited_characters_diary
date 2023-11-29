6b24a9dea917c7330bcf8e03bd427e491662c0ca
haterain0203 <haterain0203@gmail.com>
2023-11-29 13:01:34 +0900

    appInfoServiceクラスの修正

--- lib/feature/app_info/app_info_service.dart
+++ lib/feature/app_info/app_info_service.dart
@@ -1,16 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-final appInfoServiceProvider = Provider((ref) => AppInfoService());
-
-class AppInfoService {
-  Future<PackageInfo> fetchPackageInfo() async {
-    final packageInfo = await PackageInfo.fromPlatform();
-    return packageInfo;
-  }
-}
-
-final appInfoProvider = FutureProvider<PackageInfo>((ref) async {
-  final appInfoService = ref.watch(appInfoServiceProvider);
-  return appInfoService.fetchPackageInfo();
-});
+final appInfoProvider =
+    Provider.autoDispose<PackageInfo>((ref) => throw UnimplementedError());
