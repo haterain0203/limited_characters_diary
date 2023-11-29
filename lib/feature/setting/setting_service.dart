542c9c6564ab904bcb71b67165987b2f25e955a7
haterain0203 <haterain0203@gmail.com>
2023-11-29 14:02:47 +0900

    問い合わせフォームのqueryにdevice情報追加

--- lib/feature/setting/setting_service.dart
+++ lib/feature/setting/setting_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,6 +14,7 @@ final settingServiceProvider = Provider.autoDispose(
   (ref) => SettingService(
     user: ref.watch(userStateProvider).valueOrNull,
     appInfo: ref.watch(appInfoProvider),
+    deviceInfo: ref.watch(deviceInfoProvider),
   ),
 );
 
@@ -20,22 +22,38 @@ class SettingService {
   const SettingService({
     required this.user,
     required this.appInfo,
+    required this.deviceInfo,
   });
   final User? user;
   final PackageInfo appInfo;
+  final BaseDeviceInfo deviceInfo;
+
   Future<String> createContactUsUrl() async {
     const entryIdOfAppVersion = 'entry.740690739';
     const entryIdOfPlatform = 'entry.1589033762';
+    const entryIdOfOsVersion = 'entry.870640106';
+    const entryIdOfDeviceName = 'entry.1558888468';
     const entryIdOfUuid = 'entry.200390007';
-    final appVersion =
-        '$entryIdOfAppVersion=${appInfo.version}(${appInfo.buildNumber})';
-    final platform = '$entryIdOfPlatform=${Platform.operatingSystem}';
-    final uuid = '$entryIdOfUuid=${user?.uid ?? 'null'}';
 
-    final queryParams = '$appVersion&$platform&$uuid';
+    final appVersion = '${appInfo.version}(${appInfo.buildNumber})';
+    final appVersionParam = '$entryIdOfAppVersion=$appVersion';
+
+    final platform = Platform.operatingSystem;
+    final platformParam = '$entryIdOfPlatform=$platform';
+
+    final osVersion = await deviceInfo.data['systemVersion'];
+    final osVersionParam = '$entryIdOfOsVersion=$osVersion';
+
+    final deviceName = await deviceInfo.data['name'];
+    final deviceNameParam = '$entryIdOfDeviceName=$deviceName';
+
+    final uuid = user?.uid ?? 'null';
+    final uuidParam = '$entryIdOfUuid=$uuid';
+
+    final queryParams = '$appVersionParam&$platformParam'
+        '&$osVersionParam&$deviceNameParam&$uuidParam';
 
     final url = '${ConstantString.googleFormBaseUrl}&$queryParams';
-    debugPrint('url = $url');
 
     return url;
   }
