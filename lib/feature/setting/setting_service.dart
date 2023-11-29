6f5d95db3e7a4e0af2721129c3e4b9bc43f39341
haterain0203 <haterain0203@gmail.com>
2023-11-29 14:13:39 +0900

    文字列の取り扱い修正

--- lib/feature/setting/setting_service.dart
+++ lib/feature/setting/setting_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:limited_characters_diary/confidential.dart';
 import 'package:limited_characters_diary/feature/app_info/app_info_service.dart';
 import 'package:limited_characters_diary/feature/auth/auth_service.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -28,26 +29,20 @@ class SettingService {
   final BaseDeviceInfo deviceInfo;
 
   Future<String> createContactUsUrl() async {
-    const entryIdOfAppVersion = 'entry.740690739';
-    const entryIdOfPlatform = 'entry.1589033762';
-    const entryIdOfOsVersion = 'entry.870640106';
-    const entryIdOfDeviceName = 'entry.1558888468';
-    const entryIdOfUuid = 'entry.200390007';
-
     final appVersion = '${appInfo.version}(${appInfo.buildNumber})';
-    final appVersionParam = '$entryIdOfAppVersion=$appVersion';
+    final appVersionParam = '${Confidential.entryIdOfAppVersion}=$appVersion';
 
     final platform = Platform.operatingSystem;
-    final platformParam = '$entryIdOfPlatform=$platform';
+    final platformParam = '${Confidential.entryIdOfPlatform}=$platform';
 
     final osVersion = await deviceInfo.data['systemVersion'];
-    final osVersionParam = '$entryIdOfOsVersion=$osVersion';
+    final osVersionParam = '${Confidential.entryIdOfOsVersion}=$osVersion';
 
     final deviceName = await deviceInfo.data['name'];
-    final deviceNameParam = '$entryIdOfDeviceName=$deviceName';
+    final deviceNameParam = '${Confidential.entryIdOfDeviceName}=$deviceName';
 
     final uuid = user?.uid ?? 'null';
-    final uuidParam = '$entryIdOfUuid=$uuid';
+    final uuidParam = '${Confidential.entryIdOfUuid}=$uuid';
 
     final queryParams = '$appVersionParam&$platformParam'
         '&$osVersionParam&$deviceNameParam&$uuidParam';
