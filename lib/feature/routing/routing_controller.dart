38eac154b7ae0b193e2cada3ca4fb7a5b88a759c
haterain0203 <haterain0203@gmail.com>
2024-01-03 08:00:08 +0900

    RoutingControllerにHomePageへの遷移処理を追加

--- lib/feature/routing/routing_controller.dart
+++ lib/feature/routing/routing_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:limited_characters_diary/feature/link_with_social_account/link_with_social_account_page.dart';
 import 'package:limited_characters_diary/feature/setting/setting_page.dart';
+import 'package:limited_characters_diary/home_page.dart';
 import 'package:limited_characters_diary/scaffold_messenger_controller.dart';
 
 import '../../../constant/constant_string.dart';
@@ -15,7 +16,7 @@ final routingControllerProvider =
 );
 
 /// `RoutingController` はアプリ内のナビゲーションを管理するクラスです。
-/// 
+///
 /// このクラスは、さまざまなページへの遷移を担当し、ナビゲーションロジックを集約します。
 class RoutingController {
   RoutingController({required this.navigatorKey});
@@ -23,7 +24,7 @@ class RoutingController {
   final GlobalKey<NavigatorState> navigatorKey;
 
   /// 利用規約ページへ遷移します。
-  /// 
+  ///
   /// WebViewを使用して利用規約の内容を表示します。
   Future<void> goTermsOfServiceOnWebView() async {
     await Navigator.push(
@@ -39,7 +40,7 @@ class RoutingController {
   }
 
   /// お問い合わせページへ遷移します。
-  /// 
+  ///
   /// 引数 `url` で指定されたURLをWebViewで表示します。
   Future<void> goContactUsOnWebView({required String url}) async {
     await Navigator.push(
@@ -55,7 +56,7 @@ class RoutingController {
   }
 
   /// プライバシーポリシーページへ遷移します。
-  /// 
+  ///
   /// WebViewを使用してプライバシーポリシーの内容を表示します。
   Future<void> goPrivacyPolicyOnWebView() async {
     await Navigator.push(
@@ -67,6 +68,31 @@ class RoutingController {
         ),
         settings: const RouteSettings(name: '/privacyPolicy'),
       ),
+    );
+  }
+
+  /// Homeページ（縦型カレンダーページ）へ遷移します。
+  Future<void> goHomePage() async {
+    await Navigator.push(
+      navigatorKey.currentContext!,
+      MaterialPageRoute<void>(
+        builder: (_) => const HomePage(),
+        settings: const RouteSettings(name: '/'),
+      ),
+    );
+  }
+
+  /// Homeページ（縦型カレンダーページ）へ遷移し、ナビゲーションスタック上のすべてのルートを削除します。
+  ///
+  /// これはユーザーがログアウトした後に使用され、以前のページに戻ることができないようにします。
+  Future<void> goAndRemoveUntilHomePage() async {
+    await Navigator.pushAndRemoveUntil(
+      navigatorKey.currentContext!,
+      MaterialPageRoute<void>(
+        builder: (_) => const HomePage(),
+        settings: const RouteSettings(name: '/'),
+      ),
+      (Route<dynamic> route) => false, // すべてのルートを削除する条件
     );
   }
 
@@ -93,7 +119,7 @@ class RoutingController {
   }
 
   /// ログインページへ遷移し、ナビゲーションスタック上のすべてのルートを削除します。
-  /// 
+  ///
   /// これはユーザーがログアウトした後に使用され、以前のページに戻ることができないようにします。
   Future<void> goAndRemoveUntilLoginPage() async {
     await Navigator.of(navigatorKey.currentContext!).pushAndRemoveUntil(
