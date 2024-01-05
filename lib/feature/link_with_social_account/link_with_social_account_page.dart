b33f4ffba2d4ce6afd13e87ceefb931b29921e30
haterain0203 <haterain0203@gmail.com>
2024-01-05 20:21:03 +0900

    アカウント連携ページの修正

--- lib/feature/link_with_social_account/link_with_social_account_page.dart
+++ lib/feature/link_with_social_account/link_with_social_account_page.dart
@@ -1,15 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:limited_characters_diary/component/dialog_utils.dart';
 import 'package:limited_characters_diary/constant/enum.dart';
-import 'package:limited_characters_diary/feature/auth/auth_controller.dart';
-import 'package:limited_characters_diary/feature/auth/widget/login_button_google.dart';
+import 'package:limited_characters_diary/feature/auth/widget/auth_button.dart';
 
 import '../auth/auth_service.dart';
-import '../auth/widget/login_button_apple.dart';
 
 class LinkWithSocialAccountPage extends HookConsumerWidget {
   const LinkWithSocialAccountPage({super.key});
@@ -21,118 +17,54 @@ class LinkWithSocialAccountPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('アカウント連携'),
       ),
-      body: linkedProviders.isEmpty
-          ? const _NotLinked()
-          : _Linked(
-              signedInMethodList: linkedProviders,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'アカウント連携することで、\n'
+                '・アプリを誤って削除した場合のデータ復帰\n'
+                '・機能変更時のデータ移行\n'
+                'が可能になります。',
+              ),
             ),
-    );
-  }
-}
-
-class _NotLinked extends HookConsumerWidget {
-  const _NotLinked();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'アカウント登録することで、\n'
-              '・アプリを誤って削除した場合のデータ復帰\n'
-              '・機能変更時のデータ移行\n'
-              'が可能になります。',
-            ),
-          ),
-          const SizedBox(height: 32),
-          // Googleで続けるボタン
-          LoginButtonGoogle.link(
-            onPressed: () async {
-              await ref.read(authControllerProvider).linkUserSocialLogin(
-                    signInMethod: SignInMethod.google,
-                  );
-              ref.invalidate(linkedProvidersProvider);
-            },
-          ),
-          // Appleで続けるボタン
-          if (Platform.isIOS)
-            Column(
-              children: [
-                const SizedBox(height: 16),
-                LoginButtonApple.link(
-                  onPressed: () async {
-                    await ref.read(authControllerProvider).linkUserSocialLogin(
-                          signInMethod: SignInMethod.apple,
-                        );
-                    ref.invalidate(linkedProvidersProvider);
-                  },
-                ),
-              ],
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-class _Linked extends HookConsumerWidget {
-  const _Linked({required this.signedInMethodList});
-
-  final List<SignInMethod> signedInMethodList;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: List.generate(signedInMethodList.length, (index) {
-              final signedInMethod = signedInMethodList[index];
-              return ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                tileColor: Colors.white,
-                title: const Text('連携済み'),
-                trailing: _getIcon(signedInMethod: signedInMethod),
-              );
-            }),
-          ),
-          InkWell(
-            onTap: () async {
-              await ref.read(dialogUtilsControllerProvider).showYesNoDialog(
-                    desc: '連携解除してよろしいですか？',
-                    yesButtonOnPress: () async {
-                      await ref
-                          .read(authControllerProvider)
-                          .unLinkUserSocialLogin(
-                            // TODO: 複数の認証を連携できるようにする場合、要改善
-                            signInMethod: signedInMethodList.first,
-                          );
-                      ref.invalidate(linkedProvidersProvider);
-                    },
-                    buttonYesText: '連携解除',
-                  );
-            },
-            child: const ListTile(
-              leading: FaIcon(FontAwesomeIcons.arrowRightFromBracket),
-              title: Text('連携を解除する'),
-            ),
-          ),
-        ],
+            const SizedBox(height: 32),
+            // Google
+            _isLinked(
+              linkedProviders: linkedProviders,
+              signInMethod: SignInMethod.google,
+            )
+                ? const AuthButton.googleUnLink()
+                : const AuthButton.googleLink(),
+            // Apple
+            if (Platform.isIOS)
+              Column(
+                children: [
+                  const SizedBox(height: 16),
+                  _isLinked(
+                    linkedProviders: linkedProviders,
+                    signInMethod: SignInMethod.apple,
+                  )
+                      ? const AuthButton.appleUnLink()
+                      : const AuthButton.appleLink(),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
 
-  FaIcon _getIcon({required SignInMethod signedInMethod}) {
-    if (signedInMethod == SignInMethod.apple) {
-      return const FaIcon(FontAwesomeIcons.apple);
-    }
-    return const FaIcon(FontAwesomeIcons.google);
+  /// 指定された [SignInMethod] が、連携済みかどうかを判定する。
+  ///
+  /// 現在連携されているソーシャル認証のリストである [linkedProviders] に、
+  /// 指定された [SignInMethod] が含まれているかどうかを判定する。
+  bool _isLinked({
+    required List<SignInMethod> linkedProviders,
+    required SignInMethod signInMethod,
+  }) {
+    return linkedProviders.contains(signInMethod);
   }
 }
