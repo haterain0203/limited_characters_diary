9cd89fcb131314ce27bc70d7d8ed953b6fc35b01
haterain0203 <haterain0203@gmail.com>
2024-05-12 09:08:03 +0900

    UnderRepairDialog / ForcedUpdateDialog のエラーハンドリング修正

--- lib/feature/update_info/under_repair_dialog.dart
+++ lib/feature/update_info/under_repair_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:limited_characters_diary/component/error_page.dart';
 import 'package:limited_characters_diary/feature/update_info/update_info_service.dart';
 import 'package:sizer/sizer.dart';
 
@@ -11,37 +12,41 @@ class UnderRepairDialog extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final updateInfo = ref.watch(updateInfoProvider);
 
-    // このダイアログは、緊急時以外ユーザーに見せないもの
-    // loadingおよびエラーハンドリングは不要と考え、.whenは使っていない
-    if (updateInfo.hasError ||
-        updateInfo.value == null ||
-        !updateInfo.value!.isUnderRepair) {
-      return const SizedBox.shrink();
-    }
+    return updateInfo.when(
+      error: (e, s) {
+        return ErrorPage(error: e, stackTrace: s);
+      },
+      loading: () => const SizedBox.shrink(),
+      data: (updateInfo) {
+        if (!updateInfo.isUnderRepair) {
+          return const SizedBox.shrink();
+        }
 
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        ColoredBox(
-          color: Colors.grey.withOpacity(0.65),
-        ),
-        AlertDialog(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            ColoredBox(
+              color: Colors.grey.withOpacity(0.65),
             ),
-          ),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Text(
-              updateInfo.value!.underRepairComment,
-              style: TextStyle(
-                fontSize: 13.sp,
+            AlertDialog(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              title: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  updateInfo.underRepairComment,
+                  style: TextStyle(
+                    fontSize: 13.sp,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
